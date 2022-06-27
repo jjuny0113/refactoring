d3baa4b3ff5a1a6e2d97cdb7c6c3a6d47f5d66bc
jjun <jjun0625@github.com>
2022-06-27 21:56:23 +0900

    format 이름 유의미한 이름인 USD로 변경

--- 1/1-2.js
+++ 1/1-2.js
@@ -34,26 +34,28 @@ const volumeCreditsFor = (aPerformance) => {
   return result;
 };
 
+const USD = (aNumber) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 2,
+  }).format(aNumber/100);
+
 const statement = (invoice, plays) => {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    maximumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     //청구 내역 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${USD(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${USD(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}\n`;
 
   return result;
