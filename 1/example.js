58eaf391801b6ebeebf6148a3a86b7d50c085924
jjun <jjun0625@github.com>
2022-06-27 21:13:15 +0900

    리팩토링 예시 시작

---
+++ 1/example.js
@@ -0,0 +1,32 @@
+export const invoice = {
+    customer: "BigCo",
+    performances: [
+      {
+        playId: "hamlet",
+        audience: 55,
+      },
+      {
+        playId: "as_like",
+        audience: 35,
+      },
+      {
+        playId: "othello",
+        audience: 40,
+      },
+    ],
+  };
+  
+  export const plays = {
+    hamlet: {
+      name: "Hamlet",
+      type: "tragedy",
+    },
+    as_like: {
+      name: "As You Like IT",
+      type: "comedy",
+    },
+    othello: {
+      name: "Othello",
+      type: "tragedy",
+    },
+  };
