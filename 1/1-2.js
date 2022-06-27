cb3770c8bb9388d3387ac7f37e179f12d17e4d3c
jjun <jjun0625@github.com>
2022-06-27 21:53:27 +0900

    volumeCreditsFor 변수 이름 변경

--- 1/1-2.js
+++ 1/1-2.js
@@ -26,12 +26,12 @@ const amountFor = (aPerformance) => {
   return result;
 };
 
-const volumeCreditsFor = (perf) => {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+const volumeCreditsFor = (aPerformance) => {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
 };
 
 const statement = (invoice, plays) => {
