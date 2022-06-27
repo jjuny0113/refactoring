7dbbecf296984cda4422a7c0b4d5e4514b99bd65
jjun <jjun0625@github.com>
2022-06-27 21:17:07 +0900

    amountFor함수 변수 의미에 맞게 처리

--- 1/1-2.js
+++ 1/1-2.js
@@ -1,26 +1,26 @@
 import { invoice, plays } from "./example.js";
 const amountFor = (perf, play) => {
-    let thisAmount = 0
+    let result = 0
 
     switch (play.type) {
         case "tragedy":
-            thisAmount = 40000;
+            result = 40000;
             if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+                result += 1000 * (perf.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
+            result = 30000;
             if (perf.audience > 20) {
-                thisAmount += 10000 + 300 * (perf.audience - 20);
+                result += 10000 + 300 * (perf.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * perf.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
     }
 
-    return thisAmount
+    return result
 }
 
 const statement = (invoice, plays) => {
