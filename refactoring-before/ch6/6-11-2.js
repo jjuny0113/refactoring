ba17addaf84e0d072936769ea43afdd3bc48ac2f
jjuny <jjuny920113@gmail.com>
2022-07-17 00:03:14 +0900

    2.사용자에게 입력을 받아옴 -> 유효성 검사

--- refactoring-before/ch6/6-11-2.js
+++ refactoring-before/ch6/6-11-2.js
@@ -1,23 +1,38 @@
 import fs from 'fs';
 //1.run 함수를 만들어서 노드의 process 디펜던시 제거함
-run(process.argv)
+//2.사용자에게 입력을 받아옴 -> 유효성 검사
 const run = (args) => {
-  if (!args[2]) {
-    throw new Error('파일 이름을 입력하세요');
-  }
+  const command = parseCommand(args)
 
-  const fileName = `./${args[2]}.json`;
-  if (!fs.existsSync(fileName)) {
-    throw new Error('파일이 존재하지 않습니다');
-  }
 
-  const rawData = fs.readFileSync(fileName);
+
+
+  const rawData = fs.readFileSync(command.fileName);
   const orders = JSON.parse(rawData);
-  if (args.includes('-r')) {
+  if (command.countReadyOnly) {
     console.log(orders.filter((order) => order.status === 'ready').length);
   } else {
     console.log(orders.length);
   }
 }
+
+const parseCommand = (args) => {
+
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+  const countReadyOnly = args.includes('-r')
+  return {
+    fileName,
+    countReadyOnly
+  }
+}
+
+
+run(process.argv)
 
 
