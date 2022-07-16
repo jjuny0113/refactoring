737267e0b85f4a48e314cefcc05403c362d711c0
jjuny <jjuny920113@gmail.com>
2022-07-16 23:57:04 +0900

    run 함수를 만들어서 노드의 process 디펜던시 제거함

--- refactoring-before/ch6/6-11-2.js
+++ refactoring-before/ch6/6-11-2.js
@@ -1,18 +1,23 @@
 import fs from 'fs';
+//1.run 함수를 만들어서 노드의 process 디펜던시 제거함
+run(process.argv)
+const run = (args) => {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  if (args.includes('-r')) {
+    console.log(orders.filter((order) => order.status === 'ready').length);
+  } else {
+    console.log(orders.length);
+  }
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
-}
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
-}
