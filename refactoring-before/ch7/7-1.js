ac2544a435e550dfc03db2b184fca7105a8c96ba
jjuny <jjuny920113@gmail.com>
2022-07-17 22:26:49 +0900

    기본 리팩토링완료

--- refactoring-before/ch7/7-1.js
+++ refactoring-before/ch7/7-1.js
@@ -1,4 +1,37 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+
+
+class Organization {
+    #name
+    #country
+    #data
+    constructor(data) {
+        this.#data = data
+        this.#country = data.country
+        this.#name = data.name
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    get country() {
+        return this.#country
+    }
+
+    set name(value) {
+        this.#name = value
+    }
+
+    set country(value) {
+        this.#country = value
+    }
+
+    get rawData() {
+        return { name: this.name, country: this.country }
+    }
+}
+
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' })
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
